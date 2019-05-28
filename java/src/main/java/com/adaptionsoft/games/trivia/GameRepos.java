0564434fd13d3d30a445405d4b3dd74b0d6c56fd
DengZhiGuo <bobdeng@163.com>
2019-05-28 16:43:57 +0800

    Default Changelist

--- java/src/main/java/com/adaptionsoft/games/trivia/GameRepos.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/GameRepos.java
@@ -2,4 +2,8 @@ package com.adaptionsoft.games.trivia;
 
 public class GameRepos {
     public static GameRepo repo;
+
+    public static void print(String log){
+        repo.printHistory(log);
+    }
 }
