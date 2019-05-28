86e8cff5492cc3ad02d0e901b11371440d5c6c9e
DengZhiGuo <bobdeng@163.com>
2019-05-28 14:54:15 +0800

    Default Changelist

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Questions.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Questions.java
@@ -18,6 +18,19 @@ public class Questions {
             rockQuestions.addLast("Rock Question " + i);
         }
     }
+    
+    String getCurrentQuestion(int place){
+        if (place == 0) return "Pop";
+        if (place == 4) return "Pop";
+        if (place == 8) return "Pop";
+        if (place == 1) return "Science";
+        if (place == 5) return "Science";
+        if (place == 9) return "Science";
+        if (place == 2) return "Sports";
+        if (place == 6) return "Sports";
+        if (place == 10) return "Sports";
+        return "Rock";
+    }
 
 
     void askQuestion(String catagory) {
